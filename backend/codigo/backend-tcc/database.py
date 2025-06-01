f3f8473da512fde596bdaedf3212b00e11dc53ac
Lucas Sousa Silva <lucassousa4019@gmail.com>
2025-06-01 19:59:10 -0300

    feat: cria do backend

---
+++ backend/codigo/backend-tcc/database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DB_URL = "mysql+pymysql://root:@localhost/prototipo"
+
+
+engine = create_engine(DB_URL)
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
